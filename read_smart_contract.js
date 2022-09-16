110e5ea55667743679d12babcd2316f8a7cf95f0
shedrack <112991289+shedrackAI@users.noreply.github.com>
2022-09-17 00:25:07 +0100

    Update read_smart_contract.js

--- read_smart_contract.js
+++ read_smart_contract.js
@@ -1,6 +1,6 @@
 const { ethers } = require("ethers");
 
-const INFURA_ID = 'dbee5a9cdd864392bd99f204bd74de53'
+const INFURA_ID = 'ADD YOUR INFURA ID'
 const provider = new ethers.providers.JsonRpcProvider(`https://mainnet.infura.io/v3/${INFURA_ID}`)
 
 const ERC20_ABI = [
